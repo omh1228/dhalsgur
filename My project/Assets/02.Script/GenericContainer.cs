3a8ac6ca0b692ccf8f1052874ec9968eb275ff3d
omh1228 <pigoh041228@gmail.com>
2023-09-14 12:10:17 +0900

    Feat "Generic 싱글톤 구현"
    일반 싱글톤과 Generic 싱글톤 구현 후 Controller통해 접근

--- My project/Assets/02.Script/GenericContainer.cs
+++ My project/Assets/02.Script/GenericContainer.cs
@@ -2,28 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//제너릭 형식으로 Class 생성
 public class GenericContainer<T>
 {
-    private T[] items;
-    private int currentlndex = 0;
+    private T[] items;                  //커스텀 배열
+    private int currentIndex = 0;       //item 지금 번호    
 
-
-    public GenericContainer(int capccity)
+    public GenericContainer(int capacity)       //생성될 때 배열 갯수 설정
     {
-        items = new T[capccity];
+        items = new T[capacity];                //함수를 통해서 받아와서 배열 선언
+
     }
 
     public void Add(T item)
     {
-        if (currentlndex < items.Length)
+        if (currentIndex < items.Length)
         {
-            items[currentlndex] = item;
-            currentlndex++;
+            items[currentIndex] = item;     //받은 아이템을 번호를 통해서 배열에 넣는다
+            currentIndex++;                 //아이템 번호를 증가시킨다
         }
     }
 
     public T[] GetItems()
     {
+        //아이템 배열을 Return 함
         return items;
     }
+
 }
--- My project/Assets/02.Script/GenericContainerExample.cs
+++ My project/Assets/02.Script/GenericContainerExample.cs
@@ -4,45 +4,46 @@ using UnityEngine;
 
 public class GenericContainerExample : MonoBehaviour
 {
-    private GenericContainer<int> intContainer;
-    private GenericContainer<string> stringContainer;
-
+    private GenericContainer<int> intContainer;             //컨테이너 선언(int)
+    private GenericContainer<string> stringContainer;       //컨테이너 선언(string)
     // Start is called before the first frame update
     void Start()
     {
-        intContainer = new GenericContainer<int>(10);
-        stringContainer = new GenericContainer<string>(5);
+        intContainer = new GenericContainer<int>(10);       //10칸으로 선언  
+        stringContainer = new GenericContainer<string>(5);  //5칸으로 선언
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1))        //키보드 1을 누르면
         {
-            intContainer.Add(Random.Range(0, 100));
-            DisplayContainerItems(intContainer);
+            intContainer.Add(Random.Range(0, 100));     //0 - 100 랜덤 숫자를 컨테이너에 넣는다  
+            DisplayContainerItem(intContainer);         //함수를 통해서 Debug.Log에 보여준다
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))       //키보드 2을 누르면
         {
-            string randomString = "Item" + Random.Range(0, 100);
-            stringContainer.Add(randomString);
-            DisplayContainerItems(stringContainer);
+            string randonString = "item" + Random.Range(0, 100);        //임의의 문자열을 만들어준다
+            stringContainer.Add(randonString);          //0- 100 랜덤 문자열 컨테이너에 넣는다
+            DisplayContainerItem(stringContainer);      //함수를 통해서 Debug.Log에 보여준다
         }
     }
 
-    private void DisplayContainerItems<T>(GenericContainer<T> container)
+    //컨테이너에 담긴 값들을 보여주는 함수
+    private void DisplayContainerItem<T>(GenericContainer<T> container)
     {
-        T[] item = container.GetItems();
-        string temp = "";
-        for(int i = 0; i < item.Length; i++)
+        T[] item = container.GetItems();    //아이템 리스트를 받아온다
+        string temp = "";                   //Debug.Log에 보여질 칸 String
+        for (int i = 0; i < item.Length; i++)        //컨테이너의 모든 값을 for문으로 돌면서
         {
-            if (item[i] != null)
+            if (item[i] != null)      //값이 null 이 아닐 경우
             {
-                temp += item[i].ToString() + "/";
+                temp += item[i].ToString() + "/";       //string 형식으로 보여준다
             }
             else
             {
-                temp += "Empty / ";
+                temp += "Empty / ";         //null 일 겨웅에는 Empty 표현 해준다
             }
         }
         Debug.Log(temp);
